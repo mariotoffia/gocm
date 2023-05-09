c29d6978011687b891fafacea5722719c3e3ab8c
Mario Toffia <mario.toffia@gmail.com>
2023-05-09 08:22:08 +0200

    refactor where builder

--- cmexpr/expr/where_build.go
+++ cmexpr/expr/where_build.go
@@ -28,88 +28,82 @@ func (b *WhereExpressionBuilder) Build() *cmexpr.SelectExpression {
 		s.Where.LogicScopes = append(s.Where.LogicScopes, scope)
 	}
 
-	// Iterate nested logical expressions and add them nested Scopes expressions
-	var log func(
-		l *LogicalExprPropertyBuilders,
-		parent *cmexpr.LogicalOperatorScope,
-		op cmexpr.LogicalOperator,
-	)
-
-	log = func(
-		l *LogicalExprPropertyBuilders,
-		parent *cmexpr.LogicalOperatorScope,
-		op cmexpr.LogicalOperator,
-	) {
-		if len(l.and) == 0 && len(l.or) == 0 && l.not == nil && len(l.conditions) == 0 {
-			return
-		}
-
-		scp := &cmexpr.LogicalOperatorScope{}
-
-		for _, and := range l.and {
-			log(and, scp, cmexpr.LogicalOperatorAnd)
-		}
-
-		for _, or := range l.or {
-			log(or, scp, cmexpr.LogicalOperatorOr)
-		}
-
-		if l.not != nil {
-			log(l.not, scp, cmexpr.LogicalOperatorNot)
-		}
-
-		if parent != nil {
-			if parent.Scopes == nil {
-				parent.Scopes = map[cmexpr.LogicalOperator]*cmexpr.LogicalOperatorScope{}
-			}
-
-			parent.Scopes[op] = scp
-		} else {
-
-			if s.Where == nil {
-				s.Where = &cmexpr.WhereExpression{}
-			}
-
-			s.Where.LogicScopes = append(s.Where.LogicScopes, scp)
-		}
-
-		switch op {
-		case cmexpr.LogicalOperatorAnd:
-			expr := &cmexpr.AndExpression{}
-			for _, v := range l.conditions {
-				expr.Conditions = append(expr.Conditions, v.toExpression())
-			}
-
-			scp.AndExpressions = append(scp.AndExpressions, expr)
-		case cmexpr.LogicalOperatorOr:
-			expr := &cmexpr.OrExpression{}
-			for _, v := range l.conditions {
-				expr.Conditions = append(expr.Conditions, v.toExpression())
-			}
-
-			scp.OrExpressions = append(scp.OrExpressions, expr)
-		case cmexpr.LogicalOperatorNot:
-			if len(l.conditions) > 1 {
-				panic(fmt.Sprintf("not expression can only have one condition, got %d", len(l.conditions)))
-			}
-
-			scp.NotExpression = &cmexpr.NotExpression{
-				Condition: l.not.conditions[0].toExpression(),
-			}
-		}
-	}
-
 	for _, v := range b.and {
-		log(v, nil, cmexpr.LogicalOperatorAnd)
+		log(s, v, nil, cmexpr.LogicalOperatorAnd)
 	}
 
 	for _, v := range b.or {
-		log(v, nil, cmexpr.LogicalOperatorOr)
+		log(s, v, nil, cmexpr.LogicalOperatorOr)
 	}
 
 	if b.not != nil {
-		log(b.not, nil, cmexpr.LogicalOperatorNot)
+		log(s, b.not, nil, cmexpr.LogicalOperatorNot)
 	}
 
 	return s
+}
+
+func log(
+	s *cmexpr.SelectExpression,
+	l *LogicalExprPropertyBuilders,
+	parent *cmexpr.LogicalOperatorScope,
+	op cmexpr.LogicalOperator,
+) {
+	if len(l.and) == 0 && len(l.or) == 0 && l.not == nil && len(l.conditions) == 0 {
+		return
+	}
+
+	scp := &cmexpr.LogicalOperatorScope{}
+
+	for _, and := range l.and {
+		log(s, and, scp, cmexpr.LogicalOperatorAnd)
+	}
+
+	for _, or := range l.or {
+		log(s, or, scp, cmexpr.LogicalOperatorOr)
+	}
+
+	if l.not != nil {
+		log(s, l.not, scp, cmexpr.LogicalOperatorNot)
+	}
+
+	if parent != nil {
+		if parent.Scopes == nil {
+			parent.Scopes = map[cmexpr.LogicalOperator]*cmexpr.LogicalOperatorScope{}
+		}
+
+		parent.Scopes[op] = scp
+	} else {
+
+		if s.Where == nil {
+			s.Where = &cmexpr.WhereExpression{}
+		}
+
+		s.Where.LogicScopes = append(s.Where.LogicScopes, scp)
+	}
+
+	switch op {
+	case cmexpr.LogicalOperatorAnd:
+		expr := &cmexpr.AndExpression{}
+		for _, v := range l.conditions {
+			expr.Conditions = append(expr.Conditions, v.toExpression())
+		}
+
+		scp.AndExpressions = append(scp.AndExpressions, expr)
+	case cmexpr.LogicalOperatorOr:
+		expr := &cmexpr.OrExpression{}
+		for _, v := range l.conditions {
+			expr.Conditions = append(expr.Conditions, v.toExpression())
+		}
+
+		scp.OrExpressions = append(scp.OrExpressions, expr)
+	case cmexpr.LogicalOperatorNot:
+		if len(l.conditions) > 1 {
+			panic(fmt.Sprintf("not expression can only have one condition, got %d", len(l.conditions)))
+		}
+
+		scp.NotExpression = &cmexpr.NotExpression{
+			Condition: l.not.conditions[0].toExpression(),
+		}
+	}
 }
